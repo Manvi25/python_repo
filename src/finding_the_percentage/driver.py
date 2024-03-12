7bb16f1c45418d5223b32cc9903f037163f95ea1
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-12 15:41:34 +0530

    average claculated using the map function

--- src/finding_the_percentage/driver.py
+++ src/finding_the_percentage/driver.py
@@ -1,15 +1,10 @@
-import logging
-logging.basicConfig(level=logging.INFO,format='%(message)s')
-from utils import calculate_average
-
+from utils import finding_the_percentage
 if __name__ == '__main__':
-    n = int(input("Enter the number of students' records: "))
+    n = int(input())
     student_marks = {}
     for _ in range(n):
-        name, *line = input("Enter name and marks separated by space: ").split()
+        name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
-
-    query_name = input("Enter the name of the student to query: ")
-    result = calculate_average(student_marks, query_name)
-    logging.info(result)
+    query_name = input()
+    finding_the_percentage(student_marks, query_name)
--- src/finding_the_percentage/utils.py
+++ src/finding_the_percentage/utils.py
@@ -1,8 +1,5 @@
-
-def calculate_average(records, student_name):
-    if student_name in records:
-        marks = records[student_name]
-        average = sum(marks) / len(marks)
-        logging.info(f'{average:.2f}')
-    else:
-        logging.info("Student not found.")
+def finding_the_percentage(student_marks, query_name):
+    for key, value in student_marks.items():
+        if key == query_name:
+            average_score = sum(value) / len(value)
+    print("{:.02f}".format(average_score))
