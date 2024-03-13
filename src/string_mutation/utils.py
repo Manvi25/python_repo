8b3a4ad8ac842f1bb5e00ea1742cc7d9496da63b
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-13 18:41:05 +0530

    this file contains functions to do string mutation

--- src/string_mutation/utils.py
+++ src/string_mutation/utils.py
@@ -1,7 +1,11 @@
 import logging
 logging.basicConfig(level= logging.INFO, format= '%(message)s')
-def string_mutation(s,position,character):
+def string_mutation():
+   s = input().strip()
+   position, character = map(str, input().split())
+   pos=int(position)
    #convert the given string to list
    string_list= list(s)
-   string_list[position]= character
+   string_list[pos]= character
    logging.info(''.join(string_list))
+   return ''.join(string_list)
