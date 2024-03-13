6f11acca5ab4b627818c978f3462e28970069f58
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-13 18:40:48 +0530

    this file contains functions to do merging of strings

--- src/string_merge/utils.py
+++ src/string_merge/utils.py
@@ -1,10 +1,18 @@
 import logging
-logging.basicConfig(level= logging.INFO, format= '%(message)s')
-def merge_the_tools(s, k):
+logging.basicConfig(level= logging.INFO, format = '%(message)s')
+def merge_the_tools():
+    s = input().strip()
+    k = int(input())
+    ans = ''
     substrings = [s[i:i+k] for i in range(0, len(s), k)]
     for substring in substrings:
         unique_chars = []
         for char in substring:
             if char not in unique_chars:
                 unique_chars.append(char)
-        logging.info((''.join(unique_chars)))
+                ans += char
+        # logging.info(''.join(unique_chars))
+        ans += '\n'
+    return ans
+
+
