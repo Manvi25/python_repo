bed3c9191022a9f611e5ce65ca37dd85c2380093
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-13 12:19:21 +0530

    calendar module used

---
+++ src/calendar_module/utils.py
@@ -0,0 +1,8 @@
+import calendar
+import logging
+logging.basicConfig(level= logging.INFO , format= '%(message)s')
+def calendar_module():
+    m, d, y = map(int, input().split())
+    weekday_date = calendar.weekday(y, m, d)
+    weekday_name = calendar.day_name[weekday_date]
+    logging.info(weekday_name.upper())
