09a4049e6e40d6542bb2c88579c2162045ec9093
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-13 18:14:53 +0530

    this is a driver file for merging the string

--- src/string_merge/driver.py
+++ src/string_merge/driver.py
@@ -1,8 +1,3 @@
 from utils import merge_the_tools
-def main():
-    string = input("Enter the string: ").strip()
-    k = int(input("Enter the length of each substring: "))
-    merge_the_tools(string, k)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    merge_the_tools()
