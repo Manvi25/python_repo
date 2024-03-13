4ec0cb1d439317df0a6ecbc26505cbb3c0c1011c
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-13 18:39:36 +0530

    this file contains functions to second max

--- src/find_runner_up_score/utils.py
+++ src/find_runner_up_score/utils.py
@@ -1,6 +1,9 @@
 import logging
 logging.basicConfig(level= logging.INFO, format= '%(message)s')
-def runner_up(n):
+def runner_up():
+    a=int(input())
+    n=set(map(int, input().split()))
     n = list(n)
     n.sort()
     logging.info((n[-2]))
+    return (n[-2])
