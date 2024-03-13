4860339338ca633ebe62cf558961193cb173fc79
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-13 18:40:31 +0530

    this file contains functions to do string formatting

--- src/string_formatting/utils.py
+++ src/string_formatting/utils.py
@@ -1,12 +1,16 @@
 import logging
 
+
 def print_formatted():
     number = int(input())
     width = len(bin(number)[2:])
-    logging.basicConfig(level=logging.INFO, format='%(message)s')
+    ans = '\n'
+    #logging.basicConfig(level=logging.INFO, format='%(message)s')
     for i in range(1, number + 1):
         decimal = str(i).rjust(width)
         octal = oct(i)[2:].rjust(width)
         hexadecimal = hex(i)[2:].upper().rjust(width)
         binary = bin(i)[2:].rjust(width)
-        logging.info(f"{decimal} {octal} {hexadecimal} {binary}")
+        #logging.info(f"{decimal} {octal} {hexadecimal} {binary}")
+        ans += f"{decimal} {octal} {hexadecimal} {binary}\n"
+    return ans
