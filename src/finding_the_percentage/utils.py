2878d3635f0956142f24b626ed38af060508e0a0
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-13 18:40:04 +0530

    this file contains functions to find the percentage

--- src/finding_the_percentage/utils.py
+++ src/finding_the_percentage/utils.py
@@ -1,26 +1,17 @@
 import logging
-logging.basicConfig(level=logging.INFO, format='%(message)s')
-def finding_the_percentage():
-    n = int(input().strip())
+def findpercentage():
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+    n = int(input())
     student_marks = {}
     for _ in range(n):
-        name, *line = input().strip().split()
+        name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
-    return student_marks
-
-def calculate_average_marks(student_marks, query_name):
-    if query_name in student_marks:
-        avg_marks = sum(student_marks[query_name]) / len(student_marks[query_name])
-        return avg_marks
-    else:
-        logging.error("Student name not found in records")
-
-def average_marks():
-    student_marks = finding_the_percentage()
-    query_name = input().strip()
-    avg_marks = calculate_average_marks(student_marks, query_name)
-    if avg_marks:
-        logging.info(f"{avg_marks:.2f}")
-    else:
-        logging.error("Failed to calculate average marks")
+    query_name = input()
+    student_name = student_marks[query_name]
+    percentage = 0
+    for i in student_name:
+        percentage += i
+    percentage = percentage / 3
+    logging.info("%.2f" % percentage)
+    return percentage
