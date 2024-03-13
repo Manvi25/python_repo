e88b6ab093c2753472f8817ff7d15050a58e3178
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-13 10:41:19 +0530

    calculated the percentage using the map function

--- src/finding_the_percentage/driver.py
+++ src/finding_the_percentage/driver.py
@@ -1,10 +1,2 @@
-from utils import finding_the_percentage
-if __name__ == '__main__':
-    n = int(input())
-    student_marks = {}
-    for _ in range(n):
-        name, *line = input().split()
-        scores = list(map(float, line))
-        student_marks[name] = scores
-    query_name = input()
-    finding_the_percentage(student_marks, query_name)
+from src.finding_the_percentage.utils import average_marks
+average_marks()
--- src/finding_the_percentage/utils.py
+++ src/finding_the_percentage/utils.py
@@ -1,5 +1,26 @@
-def finding_the_percentage(student_marks, query_name):
-    for key, value in student_marks.items():
-        if key == query_name:
-            average_score = sum(value) / len(value)
-    print("{:.02f}".format(average_score))
+import logging
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+def finding_the_percentage():
+    n = int(input().strip())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().strip().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    return student_marks
+
+def calculate_average_marks(student_marks, query_name):
+    if query_name in student_marks:
+        avg_marks = sum(student_marks[query_name]) / len(student_marks[query_name])
+        return avg_marks
+    else:
+        logging.error("Student name not found in records")
+
+def average_marks():
+    student_marks = finding_the_percentage()
+    query_name = input().strip()
+    avg_marks = calculate_average_marks(student_marks, query_name)
+    if avg_marks:
+        logging.info(f"{avg_marks:.2f}")
+    else:
+        logging.error("Failed to calculate average marks")
