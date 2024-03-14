51dea161c5da0740db20a35a119f79c4df1f9a6e
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-14 18:55:34 +0530

    this is the utils file for floor ceil and rint

--- src/floor_ceil_rint/utils.py
+++ src/floor_ceil_rint/utils.py
@@ -1,17 +1,15 @@
-import numpy as np
+import numpy
 import logging
 
-logging.basicConfig(level=logging.INFO ,format='%(message)s')
+logging.basicConfig(level=logging.INFO,format='%(message)s')
 def floor_ceil():
-
-    np.set_printoptions(legacy='1.13')
-    n="1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8 9.9"
-
-    new_arr=list(float(i) for i in n.split(' '))
-
-    n_f=np.floor(new_arr)
-    n_c=np.ceil(new_arr)
-    n_r=np.rint(new_arr)
-
-    logging.info("%s \n%s \n%s \n",n_f,n_c,n_r)
-    return n_f,n_c,n_r
+    log_messages = []
+    numpy.set_printoptions(legacy='1.13')
+    A = numpy.array(list(map(float, input().split())))
+    logging.info(f"{numpy.floor(A)}")
+    logging.info(f"{numpy.ceil(A)}")
+    logging.info(f"{numpy.rint(A)}")
+    log_messages.append(f"{numpy.floor(A)}")
+    log_messages.append(f"{numpy.ceil(A)}")
+    log_messages.append(f"{numpy.rint(A)}")
+    return '\n'.join(log_messages)
