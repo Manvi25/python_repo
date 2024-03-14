7c1a808f0a3ce51c4b973ea9a2d74b54b64c4432
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-14 18:55:08 +0530

    this is the testcase for floor ceil and rint

--- test/test_floor_ceil_rint/floor_ceil_rint_test.py
+++ test/test_floor_ceil_rint/floor_ceil_rint_test.py
@@ -1,10 +1,24 @@
 import unittest
 from src.floor_ceil_rint.utils import floor_ceil
 
-
-class FloorCeil(unittest.TestCase):
+class FloorCeilRintTest(unittest.TestCase):
     def test_testcase1(self):
-        self.assertEqual(floor_ceil(), '''[1.  2.  3.  4.  5.  6.  7.  8.  9.]
-[2.   3.   4.   5.   6.   7.   8.   9.  10.]
-[1.   2.   3.   4.   6.   7.   8.   9.  10.]''')
-'''1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8 9.9'''
+        self.assertEquals(floor_ceil(),    '''[ 1.  2.  3.  4.  5.  6.  7.  8.  9.]
+[  2.   3.   4.   5.   6.   7.   8.   9.  10.]
+[  1.   2.   3.   4.   6.   7.   8.   9.  10.]''')
+    '''1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8 9.9'''
+
+
+    def test_testcase2(self):
+        self.assertEquals(floor_ceil(), '''[ 2.  3.  4.  5.  6.  7.  8.]
+[ 3.  4.  5.  6.  7.  8.  9.]
+[ 2.  3.  4.  6.  7.  8.  9.]''')
+
+    '''2.2 3.3 4.4 5.5 6.6 7.7 8.8
+'''
+
+    def test_testcase3(self):
+        self.assertEquals(floor_ceil(), '''[ 1.  2.  3.  4.  5.  6.  7.  8.]
+[ 2.  3.  4.  5.  6.  7.  8.  9.]
+[ 1.  2.  3.  4.  6.  7.  8.  9.]''')
+    '''1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8'''
