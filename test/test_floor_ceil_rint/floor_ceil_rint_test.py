157ad5437562d59bbb99d8649667d24583696033
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-14 18:12:54 +0530

    this is a testcase file for floor ceil and rint

---
+++ test/test_floor_ceil_rint/floor_ceil_rint_test.py
@@ -0,0 +1,10 @@
+import unittest
+from src.floor_ceil_rint.utils import floor_ceil
+
+
+class FloorCeil(unittest.TestCase):
+    def test_testcase1(self):
+        self.assertEqual(floor_ceil(), '''[1.  2.  3.  4.  5.  6.  7.  8.  9.]
+[2.   3.   4.   5.   6.   7.   8.   9.  10.]
+[1.   2.   3.   4.   6.   7.   8.   9.  10.]''')
+'''1.1 2.2 3.3 4.4 5.5 6.6 7.7 8.8 9.9'''
